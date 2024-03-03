e2ff621c603554c99fa84b764da3d4d704e0a996
luv118jz <162023138+luv118jz@users.noreply.github.com>
2024-03-03 21:10:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching **haven | M&M**"];
 
 
 let currentIndex = 0;
